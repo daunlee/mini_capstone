da978412ade0e91dace10644ff09cb7b931b43e3
daunlee <34513510+daunlee@users.noreply.github.com>
2019-06-12 13:39:35 -0400

    Add files via upload

--- gamestate.py
+++ gamestate.py
@@ -1,4 +1,5 @@
 from statenode import StateNode
+from copy import deepcopy
 is_king = False
 
 
@@ -30,6 +31,7 @@ class GameState(StateNode):
             grid = []
             # max_r, max_c = [int(x) for x in file.readline().split()]
             # init_r, init_c = [int(x) for x in file.readline().split()]
+            first_player = int(file.readline())
             for i in range(GameState.num_rows):
                 row = list(file.readline().strip()) # or file.readline().split()
                 assert (len(row) == GameState.num_rows)
@@ -42,11 +44,12 @@ class GameState(StateNode):
                         GameState.positions_red.append([i, j])
 
             grid = tuple(grid) # grid is a tuple of tuples - a 2d grid!
-            return GameState(position = (init_r, init_c).,
+            return GameState(GameState.positions_black, GameState.positions_red,
                                 grid = grid,
                                 last_action = None,
                                 parent = None,
-                                path_length = 0)
+                                path_length = 0,
+                                current_player = first_player)
 
     """
     Creates a CheckeState node.
@@ -59,12 +62,14 @@ class GameState(StateNode):
             (the initial state's parent should be None)
     path_cost (optional), the cost of the entire path to reach the state
     """
-    def __init__(self, position, grid, last_action, parent, path_length) :
+    def __init__(self, positions_black, positions_red, grid, last_action, parent, path_length, current_player) :
 
         super().__init__(parent, path_length)
-        #self.my_r, self.my_c = position[0], position[1]
+        self.positions_black = positions_black
+        self.positions_red = positions_red
         self.grid = grid
         self.last_action = last_action
+        self.current_player = current_player
 
 
 
@@ -86,7 +91,8 @@ class GameState(StateNode):
     """
     # Override
     def is_goal_state(self) :
-        return (self.grid[self.my_r][self.my_c] == 'G')
+        if (self.positions_black.length() == 0 or self.positions_red.length() == 0)
+            return True
 
     """
     Return a string representation of the State
@@ -104,63 +110,128 @@ class GameState(StateNode):
     that results in transitioning to this StateNode
     along the path taken to reach this state.
     (None if the initial state)
+    
     """
     # Override
     def describe_previous_action(self) :
         return self.last_action
 
-    """
-    Generate and return an iterable (e.g. a list) of all possible neighbor
-    states (StateNode objects).
-    If avoid_backtrack is True, don't include the parent state in the iterable.
-    """
-    # Override
-    def generate_next_states(self) :
-        states = []
-        for dr, dc in RoombaRouteState.NEIGHBORING_STEPS:
-            new_r, new_c = self.my_r + dr, self.my_c + dc
-            # Don't use any out-of-bounds moves
-            if (new_r < 0) or (new_c < 0) or (new_r >= self.get_height()) or (new_c >= self.get_width()):
+    def total_next_black_states(self):
+        """ returns a list of possible moves for all black pieces"""
+        next_black_states_list = []
+
+        for black in self.positions_black:
+            next_black_states_list.append(self.single_next_black_state(black))
+
+        return next_black_states_list
+
+    def total_next_red_states(self):
+        """ returns a list of possible moves for all red pieces"""
+        next_red_states_list = []
+
+        for red in self.positions_red:
+            next_red_states_list.append(self.single_next_red_state(red))
+
+        return next_red_states_list
+
+    def single_next_black_states(self, cur_piece_pos):
+        """ returns a list of possible moves for a single black piece"""
+        neighboring_states = [(1, -1), (-1,-1)]
+        return self.generate_next_states(cur_piece_pos, neighboring_states)
+
+    def single_next_red_states(self, cur_piece_pos):
+        """ returns a list of possible moves for a single black piece"""
+        neighboring_states = [(1, 1), (-1,1)]
+        return self.generate_next_states(cur_piece_pos, neighboring_states)
+
+    def generate_next_states(self, cur_piece_pos, possible_pos):
+        BLACK = 1
+        RED = 0
+        for move_pos in possible_pos:
+            # regular moving
+            next_state_x = cur_piece_pos[0] + move_pos[0]
+            next_state_y = cur_piece_pos[1] + move_pos[1]
+
+            # stops if the move would place the piece out of bounds
+            if (next_state_x >= self.get_dimension() or next_state_y >= self.get_dimension()):
                 continue
-            terrain = self.grid[new_r][new_c]
-            # Don't add moves that go into walls
-            if terrain == WALL:
-                continue
 
-            step_cost = RoombaRouteState.PATH_COSTS[terrain]
-            next_state = RoombaRouteState( position = (new_r, new_c),
-                                    grid = self.grid,
-                                    last_action = RoombaRouteState.NEIGHBORING_STEPS[(dr, dc)],
-                                    parent = self,
-                                    path_length = self.path_length + 1,
-                                    path_cost = self.path_cost + step_cost)
+            next_state_pos = (next_state_x, next_state_y)
 
+            # booleans for whether or not there is a piece in the way of moving
+            target_is_black = next_state_pos in self.get_position_black()
+            target_is_red = next_state_pos in self.get_position_red()
 
-            states.append(next_state)
+            # if the move is complete and there is nothing in the way
+            if (not target_is_black or not target_is_red):
+                return next_state_pos
 
-        return states
+            # there is a piece in the target area, so we can possibly jump
+            else:
+                # if the piece in the way is the same color as the current player, then don't jump"""
+                if ((self.get_current_player() == BLACK and target_is_black) or (self.get_current_player() == RED and target_is_red)):
+                    continue
+
+                else:
+                    # jump
+                    jump_x = next_state_x[0] + move_pos[0]
+                    jump_y = next_state_y[1] + move_pos[1]
+
+                    # stops if the next move is going to be out of bounds
+                    if (jump_x < 0 or jump_x > self.get_dimension() or jump_y < 0 or jump_y < 0 or jump_y > self.get_dimension()):
+                        continue
+
+                    jump_pos = (jump_x, jump_y)
+                    jump_is_black = jump_pos in self.get_position_black()
+                    jump_is_red = jump_pos in self.get_position_red()
+
+                    if (not jump_is_black or not jump_is_red):
+                        return jump_pos
+
+    def generate_next_game_state(self, action):
+        BLACK = 1
+        RED = 0
+
+        # in this case, action would be the position of where the piece would jump
+        newboard = deepcopy(self.grid)
+        action_x = action[0]
+        action_y = action[1]
+
+        newboard[action_x][action_y] = self.current_player
+
+        # if it is the black
+        if self.current_player == 1:
+            self.positions_black.append(action)
+
+        else:
+            self.positions_red.append(action)
+
+        return GameState(positions_black = self.positions_black,
+                         positions_red = self.positions_red,
+                         grid = newboard,
+                         last_action = action,
+                         parent = self,
+                         path_length = self.path_length + 1,
+                         current_player =  self.current_player % 2 + 1)
+
 
     """ Additional accessor methods used the GUI """
 
     """
-    Returns the width (number of cols) of the maze
-    """
-    def get_width(self):
-        return len(self.grid[0])
-
-    """
-    Returns the height (number of rows) of the maze
-    """
-    def get_height(self):
-        return len(self.grid)
-
-    """
     Returns a 2d tuple grid of the maze.
     """
+
+    def get_dimension(self):
+        return 8
+
     def get_grid(self) :
         return self.grid
-    """
-    Returns a 2-tuple of the roomba's position (row, col) in the maze
-    """
-    def get_position(self):
-        return self.my_r, self.my_c
+
+    def get_position_black(self):
+        return self.positions_black
+
+    def get_position_red(self):
+        return self.positions_red
+
+    def get_current_player(self):
+        return self.current_player
